908ad6ce4f7ec617f1d9fc536645caa559452ad0
amirhossein aghnia <aghniaamirhossein@gmail.com>
2022-08-28 14:03:36 +0430

    feat: add group chat apis

--- src/api/dialogue/routes/custom.js
+++ src/api/dialogue/routes/custom.js
@@ -12,6 +12,33 @@ module.exports = {
       path: "/custom/dialogues",
       handler: "custom.create",
     },
+
+    {
+      method: "POST",
+      path: "/custom/dialogues/group",
+      handler: "group.create",
+    },
+    {
+      method: "POST",
+      path: "/custom/dialogues/group/join",
+      handler: "group.join",
+    },
+    {
+      method: "POST",
+      path: "/custom/dialogues/group/add",
+      handler: "group.add",
+    },
+    {
+      method: "POST",
+      path: "/custom/dialogues/group/left",
+      handler: "group.left",
+    },
+    {
+      method: "POST",
+      path: "/custom/dialogues/group/remove",
+      handler: "group.remove",
+    },
+
     {
       method: "POST",
       path: "/custom/dialogues/:uuid/chats",
